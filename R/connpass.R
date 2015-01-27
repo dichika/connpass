c4d16cdf1e5f89175636bcda24343c2384b9e93d
dichika <ichikawa@hcc-jp.com>
2015-01-28 01:37:57 +0900

    tweaks

--- R/connpass.R
+++ R/connpass.R
@@ -1,6 +1,5 @@
 #' @export
 getInfo <- function(event_id){
-  require("rvest")
   u <- sprintf("http://connpass.com/api/v1/event/?event_id=%s", event_id)
   res <- jsonlite::fromJSON(u)
   return(res)
